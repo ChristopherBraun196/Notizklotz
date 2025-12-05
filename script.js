c344b1bbb2597daf404ffb3da00c1c8b3fca6472
Chrito196 <Christopher-Braun@outlook.com>
2025-12-05 07:05:43 +0100

    trash

--- script.js
+++ script.js
@@ -2,41 +2,59 @@
 // ich brauche notizen
 let notes = ["banana", "rasen mähen"];
 
+let trashNotes = [];
+
 function renderNotes() {
   // ich muss definieren wo sie anzuzeigen sind
   let contentRef = document.getElementById("content");
   contentRef.innerHTML = "";
 
   for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-    const note = notes[indexNote];
-    contentRef.innerHTML += getNoteTemplate (note);
+    contentRef.innerHTML += getNoteTemplate(indexNote);
+  }
+}
+function getNoteTemplate(indexNote) {
+  return `<p>+ ${notes[indexNote]}<button onclick="deleteNote(${indexNote})">X</button></p>`;
+}
+
+function renderTrashNotes() {
+  // ich muss definieren wo sie anzuzeigen sind
+  let trashContentRef = document.getElementById("trash_content");
+  trashContentRef.innerHTML = "";
+
+  for (
+    let indexTrashNote = 0;
+    indexTrashNote < trashNotes.length;
+    indexTrashNote++
+  ) {
+    trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
   }
 }
 
-function getNoteTemplate(note){
-    return  `<p>+ ${note}</p>`
+function getTrashNoteTemplate(indexTrashNote) {
+  return `<p>+ ${trashNotes[indexTrashNote]}<button onclick="deleteTrashNote(${indexTrashNote})">X</button></p>`;
 }
-
 // notizen hinzufügen
 
-function addNote(){
-    let noteInputRef = document.getElementById('note_input')
-    let noteInput = noteInputRef.value; 
+function addNote() {
+  let noteInputRef = document.getElementById("note_input");
+  let noteInput = noteInputRef.value;
 
-    notes.push(noteInput);
+  notes.push(noteInput);
 
-    renderNotes();
+  renderNotes();
 
-    noteInputRef.value = "";
+  noteInputRef.value = "";
 }
 
-// eingabe von User definieren 
-// eingabe auslesen
-// eingabe speichern 
-// eingabe anzeigen lassen
+function deleteNote(indexNote) {
+  let trashNote = notes.splice(indexNote, 1)[0];
+  trashNotes.push(trashNote);
+  renderNotes();
+  renderTrashNotes();
+}
 
-
-
-
-// notizen löschen
-//  -> notizen archivieren
+function deleteTrashNote(indexTrashNote){
+  trashNotes.splice(indexTrashNote, 1);
+  renderTrashNotes();
+}
